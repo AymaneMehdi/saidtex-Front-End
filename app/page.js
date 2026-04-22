5f26909686d3debbc878bcc13ac1ba2f8824c2d0
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-22 22:57:49 +0100

    Add Contact component and integrate it into the Home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Hero from "./components/hero";
 import About from "./components/about";
 import Partners from "./components/partenaires";
 import Whyus from "./components/whyus";
+import Contact from "./components/contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Partners />
       <Whyus />
+      <Contact />
     </div>
   );
 }
