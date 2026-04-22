7267f4537fc6297588e9916ea9ab931fd7688e12
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-22 22:55:02 +0100

    Add About component and integrate it into the Home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/nav";
 import Hero from "./components/hero";
+import About from "./components/about";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
