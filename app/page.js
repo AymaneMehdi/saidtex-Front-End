8399b876a1f7c3bd23b7ca9d8d4b3c567bd2f81c
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-22 22:54:31 +0100

    Add Hero component and integrate it into the Home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/nav";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
