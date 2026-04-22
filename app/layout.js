16a6057d577f0515b95f645d829cdc058c39f820
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-22 22:51:48 +0100

    Add new assets and Tailwind CSS configuration
    - Added a new PNG image (textile.png) to the images directory.
    - Introduced a new SVG file (thechnique.svg) for vector graphics.
    - Created a Tailwind CSS configuration file (tailwind.config.js) with custom themes and plugins.

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "SAIDTEX",
+  description: "Saidtex, le leader de l'industrie textile au Maroc. Avec plus de 50 ans d'expertise, nous offrons des solutions de pointe en filature, finissage, tissage et bonneterie. En tant que partenaire privilégié, nous sommes fiers de servir nos clients locaux et internationaux avec un accent fort sur la qualité et le service. Explorez notre gamme de produits et bénéficiez de notre expérience inégalée dès aujourd'hui.",
+  keywords: [
+    "Machines textiles",
+    "Filature Maroc",
+    "Finissage Maroc",
+    "Bonneterie Maroc",
+    "Tissage Maroc",
+    "Équipement textile Maroc",
+    "Machines neuves",
+    "Machines anciennes",
+    "Industrie textile",
+    "Maroc",
+    "Machines Maroc",
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
